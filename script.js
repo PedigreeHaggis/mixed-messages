4a0d2b328c2d0e6fda0a880807014791fa6010d0
Gary Taylor <garycharlestaylor@outlook.com>
2022-07-17 16:55:17 +0100

    Created madlibs

---
+++ script.js
@@ -0,0 +1,6 @@
+const madLibs = {
+    _lib: ['To *VERB* a Mockingbird', 'I can\'t start my day without *NOUN*', 'I like my *NOUN* *ADJECTIVE*.'],
+    _noun: ['COFFEE', 'BED', 'CAR', 'GLASS', 'ROBOT', 'CHAIR'],
+    _verb: ['JUMP', 'RUN', 'CRASH', 'THROW', 'STROKE', 'LIFT'],
+    _adjective: ['STRONG', 'COLD', 'YELLOW', 'HOT', 'ILLUMINATING']
+}
